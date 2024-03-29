8397bcba7965b2540246273e202e2545c803200b
Whisky0808 <kyra.yli@outlook.com>
2024-03-29 16:07:07 -0400

    navigator-bottom

--- App.js
+++ App.js
@@ -1,13 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import { useCallback } from 'react';
+import * as SplashScreen from "expo-splash-screen"
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {BottomTabNavigation} from './navigation/BottomTabNavigation'
 
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator();
+
+  const[fontsLoaded] = useFonts({
+    regular:require("./assets/fonts/Poppins-Regular.ttf"),
+    bold:require("./assets/fonts/Poppins-Bold.ttf")
+  })
+
+  const onLayoutRootView = useCallback(async()=>{
+    if(fontsLoaded){
+      await SplashScreen.hideAsync();
+    }
+  },[fontsLoaded]);
+
+  if(!fontsLoaded){
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
+    
+    <>
+    {/* <View style={styles.container}>
       <Text style={styles.textStyle}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </View> */}
+    <NavigationContainer>
+      <Stack.Navigator>
+
+        <Stack.Screen
+          name='Bottom Navigation'
+          component={BottomTabNavigation}
+          options={{headerShown:false}}
+
+        />
+
+
+        
+      </Stack.Navigator>
+    </NavigationContainer>
+    </>
   );
 }
 
@@ -19,7 +60,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   textStyle:{
-    // fontFamily:"",
-    fontSize:20,
+    // fontFamily:"Monaco",
+    fontSize:10,
   }
 });
